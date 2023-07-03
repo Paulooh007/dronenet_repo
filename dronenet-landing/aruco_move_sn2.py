4c9c28acdab3f46829c558414763c3775d2891a1
Okewunmi Paul <okewunmipaul@yahoo.com>
2023-07-03 09:29:06 +0100

    Update aruco_move_sn2.py

--- dronenet-landing/aruco_move_sn2.py
+++ dronenet-landing/aruco_move_sn2.py
@@ -9,9 +9,10 @@ import sys
 
 
 w, h = 640, 480
-takeoff_height = 10
-velocity = .5
+takeoff_height = 15
+velocity = 3
 rel_alt = 0
+sleep_time = 15
 
 
 def arm():
@@ -74,11 +75,11 @@ def send_body_offset_ned_command(vx, vy):
                         the_connection.target_component, 
                         mavutil.mavlink.MAV_FRAME_LOCAL_NED, 
                         # int(0b110111111000), #pos
-                        int(0b100111000111), #vel
-                        10, 3, -10, 
+                        int(0b110111000111), #vel
+                        0, 3, -10, 
                         vx, vy, 0, 
                         0, 0, 0, 
-                        1.56, 0))
+                        0, 0))
 
     while True:
         msg = the_connection.recv_match(
@@ -138,7 +139,8 @@ def drawQuadrant():
 
 
 if (__name__ == "__main__"):
-    the_connection = mavutil.mavlink_connection('udpin:172.26.64.1:14550')
+    # the_connection = mavutil.mavlink_connection('udpin:172.26.64.1:14550')
+    the_connection = mavutil.mavlink_connection('COM3', 57600)
 
     await_heartbeat()
 
@@ -162,9 +164,10 @@ if (__name__ == "__main__"):
     while True:
         msg = the_connection.recv_match(type = "GLOBAL_POSITION_INT", blocking = False)
         if msg:
-            if msg.relative_alt >= (takeoff_height * 1000):
-                send_body_offset_ned_command(velocity, 0) #offset by some meters
-                # time.sleep(20)
+            print(msg.relative_alt)
+            if msg.relative_alt >= ((takeoff_height * 1000) - 700): #20 centimeters shy of take off height
+                send_body_offset_ned_command(velocity, 0)
+                time.sleep(sleep_time)
                 set_flight_mode("RTL")
                 break
 
@@ -172,6 +175,22 @@ if (__name__ == "__main__"):
     cap = cv2.VideoCapture(1)
 
     centerX, centerY = w//2, h//2  #Get center of Frame
+    
+    # intrinsic_camera = np.array(((933.15867, 0, 657.59),(0,933.1586, 400.36993),(0,0,1)))
+    # distortion = np.array((-0.43948,0.18514,0,0))
+    
+    intrinsic_camera = np.array(((534.9681530430366, 0, 305.39166889960603),(0,713.1386199148463, 255.72950392809662),(0,0,1)))
+    distortion = np.array((-0.16161169421208624,
+                            0.24648910664734525,
+                            0.004137344187593899,
+                            -0.0016340908890220367,
+                            -0.43766632091131))
+    
+    horizontal_res = 640
+    vertical_res = 480
+
+    horizontal_fov = 90 * (math.pi / 180 )
+    vertical_fov = 90 * (math.pi / 180)
 
     rand = random.randint(0, 1000)
     debug_image_writer = cv2.VideoWriter(f"vid_record_" + str(rand) + ".avi",cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 25.0,(w,h))
@@ -179,22 +198,15 @@ if (__name__ == "__main__"):
     while True:
         
         # frame = me.get_frame_read().frame
+        ret, frame = cap.read()
 
-        try:
-            ret, frame = cap.read()
-            frame = cv2.resize(frame, (w,h))
-
-        except Exception as e:
-            continue
-            
-
-        
+        frame = cv2.resize(frame, (w,h))
 
         msg = the_connection.recv_match(type = "GLOBAL_POSITION_INT", blocking = False)
         if msg:
             rel_alt = msg.relative_alt
         cv2.putText(frame, f"Takeoff Alt: {takeoff_height} Relative Alt: {int(rel_alt/1000)}",
-                        (20, 20), cv2.FONT_HERSHEY_SIMPLEX,
+                        (20, 50), cv2.FONT_HERSHEY_SIMPLEX,
                         0.7, (0, 255, 255), 2)  
             
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
@@ -213,10 +225,46 @@ if (__name__ == "__main__"):
             markerInfo = drawMarker(corners, ids)  
 
             cX, cY = markerInfo[0]   #Get marker midpoint
+            
+            rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners, 0.02,intrinsic_camera,distortion)
+            
+            cv2.aruco.drawDetectedMarkers(frame, corners) 
+
+            try:
+                cv2.drawFrameAxes(frame,intrinsic_camera,
+                                distortion, rvec, tvec, 0.01)
+            except Exception as e:
+                continue
+            
+            
+            x = tvec[0][0][0]
+            y = tvec[0][0][1]
+            z = tvec[0][0][2]
+            
+            y_sum = 0
+            x_sum = 0
+            
+            x_sum = corners[0][0][0][0]+ corners[0][0][1][0]+ corners[0][0][2][0]+ corners[0][0][3][0]
+            y_sum = corners[0][0][0][1]+ corners[0][0][1][1]+ corners[0][0][2][1]+ corners[0][0][3][1]
+    
+            x_avg = x_sum*.25
+            y_avg = y_sum*.25
+            
+            x_ang = round((x_avg - horizontal_res*.5)*(horizontal_fov/horizontal_res), 2)
+            y_ang =round((y_avg - vertical_res*.5)*(vertical_fov/vertical_res), 2)
+            
+            
+            
+            print(x_ang, y_ang)
+            
 
             #Calculate error i.e Distance btw aruco midpoint and frame midpoint
             error = int(distance((cX, cY), (centerX, centerY)))
             markerWidth, markerHeight = markerInfo[1]
+            
+            cv2.putText(frame, f"X_vel: {x_ang}  Y_vel: {y_ang}  Error: {error}",
+                    (20, 20), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (0, 255, 0), 2)
 
             rW, rH = round(markerWidth/w, 1), round(markerHeight/h, 1)    #Calculate ratio
             # rW, rH = 0.4, 0.4
